fb069da954a143156d0cb2bd74ceeceff34e4387
Danielle <daniellec0321@gmail.com>
2023-04-03 21:13:57 -0400

    Created finger table module

--- FingerTable.py
+++ FingerTable.py
@@ -5,11 +5,21 @@ class FingerTable():
 
     def addNode(self, args):
         # args is a tuple structured as (position, IP_ADDR, port)
+        # position is the TOP RANGE of the range
+
         if not type(args) is tuple:
             return False
+
+        # check that node is not already in table
+        for node in self.ft:
+            if node[1] == args[1]:
+                return False
+
         self.ft.append(args)
         self.ft = sorted(self.ft)
         return True
+
+
 
     def delNode(self, IP_ADDR):
         # delete node with that address
@@ -24,3 +34,14 @@ class FingerTable():
 
         del self.ft[to_delete]
         return True
+
+
+
+    def findProcess(self, position):
+        # find the process with a position that is juuuuust higher than the given position and return it
+        for node in self.ft:
+            if node[0] >= position:
+                return node
+
+        # if could not find one, then return the first node in the list
+        return self.ft[0]
