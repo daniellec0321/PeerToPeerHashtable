2d0d61e334694763eaa7b04734786cdd766e9c4c
Benjamin Egloff <begloff@student12.cse.nd.edu>
2023-04-03 21:16:53 -0400

    First methods in Client

--- P2PHashTableClient.py
+++ P2PHashTableClient.py
@@ -1,4 +1,87 @@
+import requests
+import math
+
+
 class P2PHashTableClient:
     def __init__(self):
-        pass
+        ipAddress = None # IP of client --> where it can be reached
+        port = None # What port the client can be reached at
+        prev = None # prev node in the ring
+        next = None # next node in the ring
+        highRange = None # highest radian number client is responsible for
+        lowRange = None # lowest radian number client is responsible for
+        fingerTable = None # client's finger table
+        projectName = None # project name to find in naming service
+    
+    def enterRing(self, projectName):
+        # To enter ring, need to check naming service to verify there is or isn't an existing client
+        
+        #Fetch IP and store in ipAddress variable
+        ip = requests.get('http://icanhazip.com')
+        self.ipAddress = ip.text[:-1] #Need to remove newline char
+        
+        #Store passed projectName in project name var
+        self.projectName = projectName
+        
+        #Use project name to locate server and test communication
+        if not self.locateServer():
+            self.startP2P()
+        
+    def locateServer(self):
+        
+        # Get json data from naming service and try to find existing clients
+        data = requests.get("http://catalog.cse.nd.edu:9097/query.json")
+        data = data.json()
+
+        data = list(filter( lambda x: "type" in x and "project" in x and x["type"] == "p2phashtable" and x["project"] == self.projectName, data))
+        
+        if not data:
+            #No entries in the ring, so need to start 
+            return False
+        
+        #TODO: Check to see if any servers are available
+        
+    def startP2P(self):
+        #This method creates the P2P system --> First Hash the IP addr
+        
+        hashedIP = self.hashKey(self.ipAddress)
+        
+        # Max Hash for djb2 is 2^32 - 1
+        # Calculate spot on circle by dividing hashedIP by (2^32 - 1)
+        ratio = hashedIP / (pow(2,32) - 1)
+        
+        #Multiply ratio by 2pi radians to find its exact spot on the ring
+        location = ratio * 2 * math.pi
+        
+        #Both ranges will be approx equal
+        self.highRange = location
+        
+        #Differentiates the range by the smallest fraction
+        self.lowRange = location + 0.0000000000000001
+
+    
+    def hashKey(self, key):
+        #This hashing algorithm is djb2 source: http://www.cse.yorku.ca/~oz/hash.html
+        
+        # NOTE: Max Hash -->  2^{32} - 1 = 4,294,967,295
+        
+        try:
+        
+            hashedKey = 5381
+            
+            for x in key:
+                hashedKey = (( hashedKey << 5) + hashedKey) + ord(x)
+            
+            return hashedKey & 0xFFFFFFFF
+
+        except: #Catch non strings and record as errors
+            return False
+        
+        
+        
+        
+if __name__ == '__main__':
+    client = P2PHashTableClient()
+    client.enterRing('begloff-project')
+        
     
